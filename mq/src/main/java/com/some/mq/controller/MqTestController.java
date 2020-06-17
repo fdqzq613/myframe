ee6a28c9dec64289cb6e2ffdb5ccd84e5af1a2ff
fdqzq613 <364204754@qq.com>
2020-06-17 18:18:35 +0800

    #v1

--- mq/src/main/java/com/some/mq/controller/MqTestController.java
+++ mq/src/main/java/com/some/mq/controller/MqTestController.java
@@ -1,15 +1,27 @@
 package com.some.mq.controller;
 
+import com.some.mq.consume.StreamInputChannel;
 import com.some.mq.producer.SendService;
 
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Correlation;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 
@@ -28,6 +40,8 @@ public class MqTestController {
     private AmqpTemplate amqpt;
     @Autowired
     private RabbitTemplate rabbitTemplate;
+    @Autowired
+    private AmqpAdmin rabbitAdmin;
 
     @ApiOperation(value = "send", notes = "send", httpMethod = "POST")
     @RequestMapping(value = "/send", method = {RequestMethod.POST})
@@ -77,6 +91,12 @@ public class MqTestController {
                 System.out.println("消息使用的路由键 routing : " + routingKey);
             }
         });
+
+        rabbitAdmin.declareExchange(new TopicExchange("topic.my.create.exchange",true,false));
+        rabbitAdmin.declareQueue(new Queue("my.create.queue",true));
+        //绑定队列到交换器，通过路由键
+        rabbitAdmin.declareBinding(new Binding("my.create.queue", Binding.DestinationType.QUEUE,
+                "topic.my.create.exchange","test-create-routingKey",new HashMap()));
     }
 
 
@@ -87,12 +107,26 @@ public class MqTestController {
 
         //第一个参数是交换机，第二个参数是routingKey,第三个参数是要发送的消息,支持实体对象
         //ack
-        amqpt.convertAndSend("some-input3", "test-routingKey", "myTestSome-routingKey:"+msg);
-        amqpt.convertAndSend("some-input3", "test-routingKey2", "myTestSome-routingKey2:"+msg);
+        amqpt.convertAndSend(StreamInputChannel.INPUT3, "test-routingKey", "myTestSome-routingKey:"+msg);
+        amqpt.convertAndSend(StreamInputChannel.INPUT3, "test-routingKey2", "myTestSome-routingKey2:"+msg);
+        amqpt.convertAndSend(StreamInputChannel.INPUT3, "test-routingKey", "myTestSome-routingKeydd:"+msg,new MessagePostProcessor(){
 
+            @Override
+            public Message postProcessMessage(Message message) throws AmqpException {
+                return message;
+            }
+
+            @Override
+            public Message postProcessMessage(Message message, Correlation correlation) {
+                return message;
+            }
+        });
         //不存在路由 nack
         amqpt.convertAndSend("myTestSome-no-Exchange", "some2-key", "myTestSome-no-Exchange:"+msg);
         amqpt.convertAndSend("myTestSome-Exchange", "some-no-key", "some-no-key:"+msg);
+
+        byte[] s = (byte[]) amqpt.receiveAndConvert("delay.order.input.delay-order",1000);
+        log.info(new String(s));
     }
 
 
