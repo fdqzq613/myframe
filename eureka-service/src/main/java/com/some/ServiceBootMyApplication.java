c5068c500f55318ea1cebca04687fe6e0bdccd96
fdqzq613 <364204754@qq.com>
2020-06-23 17:49:54 +0800

    #v1

--- eureka-service/src/main/java/com/some/ServiceBootMyApplication.java
+++ eureka-service/src/main/java/com/some/ServiceBootMyApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableHystrixDashboard  //http://localhost:8085/hystrix 输入http://localhost:8086/hystrix.stream//单体应用监控
+@EnableHystrixDashboard //http://localhost:8085/hystrix 输入http://localhost:8086/hystrix.stream//单体应用监控
 @EnableTurbine  //http://localhost:8085/hystrix   http://localhost:8085/turbine.stream//集群应用监控
 public class ServiceBootMyApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) throws Exception {
